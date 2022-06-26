5ece5ff85ff142d26663c9444de9f623137d7051
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-06-26 09:07:03 +0200

    useState

--- stidentfrontend/src/components/Student.js
+++ stidentfrontend/src/components/Student.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Container } from '@mui/system';
@@ -6,10 +6,13 @@ import { Paper } from '@mui/material';
 
 export default function Student() {
     const paperStyle={padding: '50px 20px', width: 600,margin: "20px auto"}
+    const[name,setName]=useState('')
+    const[adress,setAdress]=useState('')
   return (
 
     <Container>
         <Paper elevation ={3} style={paperStyle}>
+            <h1>Add Student</h1>
     <Box
       component="form"
       sx={{
@@ -18,8 +21,8 @@ export default function Student() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="standard-basic" label="Student name" variant="standard" />
-      <TextField id="standard-basic" label="Student adress" variant="standard" />
+      <TextField id="standard-basic" label="Student name" variant="standard" value={name}onChange={(e)=>setName(e.target.value)}/>
+      <TextField id="standard-basic" label="Student adress" variant="standard" value={adress}onChange={(e)=>setAdress(e.target.value)} />
     </Box>
     </Paper>
     </Container>
