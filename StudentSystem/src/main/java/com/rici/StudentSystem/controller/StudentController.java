34b12b0bad1703dcb50eece0a45dcf6f6b2d7255
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-06-26 08:19:17 +0200

    data

--- StudentSystem/src/main/java/com/rici/StudentSystem/controller/StudentController.java
+++ StudentSystem/src/main/java/com/rici/StudentSystem/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.rici.StudentSystem.controller;
 import com.rici.StudentSystem.model.Student;
 import com.rici.StudentSystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -19,5 +18,9 @@ public class StudentController {
         studentService.saveStudent(student);
         return "New student is added";
 
+    }
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
     }
 }
--- StudentSystem/src/main/java/com/rici/StudentSystem/service/StudentService.java
+++ StudentSystem/src/main/java/com/rici/StudentSystem/service/StudentService.java
@@ -2,6 +2,9 @@ package com.rici.StudentSystem.service;
 
 import com.rici.StudentSystem.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
